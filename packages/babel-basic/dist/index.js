d6b675e89240ea952390f847ff2e2fd5ad68b305
rupid <liujianwei1213@163.com>
2021-05-15 11:59:20 +0800

    feat: babel 配置文件说明

---
+++ packages/babel-basic/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+ * @Desc: 
+ * @FilePath: /tutor-babel/packages/tutor-basic/lib/index.js
+ * @Author: liujianwei1
+ * @Date: 2021-05-14 14:34:00
+ * @LastEditors: liujianwei1
+ * @Reference Desc: 
+ */
+var number1 = 10;
+var number2 = 20;
+
+var sum = function sum(num1, num2) {
+  return num1 + num2;
+};
